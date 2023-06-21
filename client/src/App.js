1508c06dd8666cab31876f0374d0d3850894ec8e
Varintorn SIthisint <130687857+Varinpond-web@users.noreply.github.com>
2023-06-21 14:55:14 +0700

    add boostrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
-import Content from './component/content';
+
+
 function App() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-      <Content/>  
+      <div class="p-3 mb-2 bg-primary text-white">Topic</div>
+      <div class="p-3 mb-2 bg-secondary text-white">Secondary</div>
+      <div class="p-3 mb-2 bg-success text-white">Third</div>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ client/src/component/nav.js
@@ -0,0 +1,16 @@
+export default function Nav() {
+
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <a href="/">Home</a>
+                </li>
+                <li>
+                    <a href="/">Profile</a>
+                </li>
+            </ul>
+        </nav>
+    )
+
+}
