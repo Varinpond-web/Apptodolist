28d181e8b2d9bce9cc45ddd739ca836e4c267bce
Varintorn SIthisint <130687857+Varinpond-web@users.noreply.github.com>
2023-06-21 14:56:10 +0700

    add double third

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ function App() {
       <div class="p-3 mb-2 bg-primary text-white">Topic</div>
       <div class="p-3 mb-2 bg-secondary text-white">Secondary</div>
       <div class="p-3 mb-2 bg-success text-white">Third</div>
+      <div class="p-3 mb-2 bg-success text-white">Third</div>
     </div>
   )
 }
