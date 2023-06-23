fdabc680684196564a3b2216cb86fdf121ef7148
Varintorn SIthisint <130687857+Varinpond-web@users.noreply.github.com>
2023-06-23 15:26:39 +0700

    update

--- client/src/App.js
+++ client/src/App.js
@@ -5,13 +5,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<Home/>} />
-          <Route path="/create" element={<Create/>} />
-        </Routes>
-      </BrowserRouter>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/create" element={<Create />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
