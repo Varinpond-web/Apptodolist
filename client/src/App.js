83ed52557766269f965b140d5456a6de13601c66
Varintorn SIthisint <130687857+Varinpond-web@users.noreply.github.com>
2023-06-23 14:28:15 +0700

    new

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
-
+import Home from './pages/Home';
+import Create from './pages/Create';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <div class="p-3 mb-2 bg-primary text-white">Topic</div>
-      <div class="p-3 mb-2 bg-secondary text-white">Secondary</div>
-      <div class="p-3 mb-2 bg-success text-white">Third</div>
-      <div class="p-3 mb-2 bg-success text-white">Third</div>
-    </div>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home/>} />
+          <Route path="/create" element={<Create/>} />
+        </Routes>
+      </BrowserRouter>
   )
 }
 
